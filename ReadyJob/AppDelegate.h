9c3c89232e73a4f136b4be04be15dadf31649363
xiayan <xyen@xiayandeMac.local>
2017-06-20 13:19:42 +0800

    Initial Commit

---
+++ ReadyJob/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReadyJob
+//
+//  Created by xiayan on 2017/6/20.
+//  Copyright © 2017年 xiayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
